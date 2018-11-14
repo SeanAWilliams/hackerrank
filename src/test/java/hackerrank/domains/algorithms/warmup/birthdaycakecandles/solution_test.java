27bb0770d5ec10d8605816b919638c580462fd69
Sean Williams <sean_williams@ultimatesoftware.com>
2018-11-14 09:57:25 -0800

    birthday candles

---
+++ src/test/java/hackerrank/domains/algorithms/warmup/birthdaycakecandles/solution_test.java
@@ -0,0 +1,12 @@
+package hackerrank.domains.algorithms.warmup.birthdaycakecandles;
+
+import org.junit.Test;
+
+public class solution_test {
+
+	@Test
+	public void birthdayCakeCandles_test () {
+			int[] ar = {3,2,1,3};
+		  Solution.birthdayCakeCandles(ar);
+	}
+}
